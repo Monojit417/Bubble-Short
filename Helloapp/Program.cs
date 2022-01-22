727a9decc182cd449002c0af17e01a97ed4ffbe8
MONOJIT <MONOJIT@DESKTOP-FDFCVQ8>
2022-01-23 01:11:06 +0530

    Add project files.

---
+++ Helloapp/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] arr = { 10, 7, 17, 13, 14, 28, 5 };
+int temp;
+for(int i = 0; i < arr.Length; i++)
+{
+    for(int j = 0; j < arr.Length - 1; j++)
+    {
+        if (arr[j] > arr[j + 1])
+        {
+            temp=arr[j];
+            arr[j]=arr[j + 1];
+            arr[j+1]=temp;
+        }
+    }
+}
+foreach(int i in arr)
+{
+    Console.Write(i + " ");
+}
